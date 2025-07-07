529b743bedeca0a61bc7a0a15592728911cfe27e
Monochrome PIe <popularmmos2548@gmail.com>
2025-07-08 03:59:03 +0700

    Stablization code is up with somewhat working autoocenter
    havent fixed the issue with system tending toward disorder

--- main/main.ino
+++ main/main.ino
@@ -26,8 +26,7 @@ volatile unsigned long lastPulseTimeDist = 0;
 double currentTime;
 double measureInterval = 10; 
 double lastMeasureTime;
-double accelerationInterval = 200;
-double lastAccelerationTime;
+
 //Constants
 double m_c = 0.2;
 double m_p = 0.03;
@@ -36,8 +35,10 @@ double g = 9.81;
 double x_max = 0.67;
 double centerDistance = x_max / 2; 
 double offsetDistance = 0.05;
-double stableOffsetDistance = 0.3;
 double targetedFinalDistance;
+double tolerable_x_min = 0.2;
+double tolerable_x_max = 0.5;
+
 
 //Variables
 double theta;
@@ -62,26 +63,32 @@ double delta_E;
 double final_dx_new;
 double ddx_new;
 double targetedFinalTheta;
+double tempTargetedFinalTheta;
+
+//use for marking beginning and ending position for swinging phase
 double x_begin;
 double x_end;
-double x_end_decel;
+
+//use for marking deceleration point in stablizing phase
+double x_i;
+double x_f;
+
 
 //Motor Control
 int motorPWM;
-int motorMaxSpeed = 255;
-int initialPWMbeforeDecelleration;
+int motorMaxSpeed;
+int initialPWMbeforeDeceleration;
 bool isMoving = false;
 bool isSwinging = false;
 bool calculated = false;
 bool beginStablization = false;
-
+bool decelerationPointMarked = false;
+bool adjustmentPeriod = true;
+bool decelerationPeriod = false;
+bool targetThetaCrossed = false;
 
 //boolean for movement control
 bool travellingFromDirection = false; //false for travelling from right side
-bool returnToCenter = true;
-bool moveToStable = false;
-bool travelFurther = false;
-bool safegaurd = false;
 
 void setup() {
   pinMode(angleEncPinA, INPUT_PULLUP);
@@ -118,7 +125,7 @@ void loop() {
 
     //setting and calculation final swinging trajectory goals for upward position for both sides
     if (!isMoving && !isSwinging && !calculated && !travellingFromDirection){
-      targetedFinalTheta  = PI;
+      targetedFinalTheta  = PI - radians(20);
       if (targetedFinalTheta > PI) targetedFinalTheta = PI;
       calculateEnergyEquation(theta, dtheta, targetedFinalTheta, x, dx, centerDistance + offsetDistance);
       isMoving = true;
@@ -126,13 +133,14 @@ void loop() {
     }
 
     if (!isMoving && !isSwinging && !calculated && travellingFromDirection){
-      targetedFinalTheta  = - PI ;
+      targetedFinalTheta  = - PI + radians(20) ;
       if (targetedFinalTheta < -PI) targetedFinalTheta = -PI;
       calculateEnergyEquation(theta, dtheta, targetedFinalTheta, x, dx, centerDistance - offsetDistance);
       isMoving = true;
       calculated = true;
     }
 
+    //only change PWM when conditions were met
     if (isMoving && !isSwinging && calculated){
       accelerateTo(final_dx_new, x, x_begin, x_end);
     }
@@ -164,54 +172,142 @@ void loop() {
     }
 
     //set flags for shifting into stablizing phase
-    if (theta >= PI){
+    if (theta >= PI - radians(2)){
       beginStablization = true;
       targetedFinalTheta = PI;
+      tempTargetedFinalTheta = targetedFinalTheta;
       calculated = false;
       isMoving = true;
       isSwinging = false;
-      returnToCenter = true;
     }
 
-    if (theta <= -PI){
+    if (theta <= -PI + radians(2)){
       beginStablization = true;
       targetedFinalTheta = -PI;
+      tempTargetedFinalTheta = targetedFinalTheta;
       calculated = false;
       isMoving = true;
       isSwinging = false;
-      returnToCenter = true;
     }
-
-    if(!isMoving){
-      motorPWM = 0;
-    }
-
-    motor(motorPWM);
 
   }
 
-  //Maintain upward position stability phase
+
+  //Maintain upward position stability at center phase
   if (beginStablization){
 
     // 0 left, 1 right; based on cart position
     int currentSide = (x < centerDistance)? 0: 1;
-    bool overstated = false;
+    double toleranceAngle = radians(0.01);
+    double offsetAngle = radians(5) * (x - centerDistance) / (centerDistance );
+    targetedFinalTheta= tempTargetedFinalTheta + offsetAngle;
+
+    motorMaxSpeed = 255 * 2.25 * abs(theta - targetedFinalTheta)/radians(10);
+
+    //checking whether pendulum crosses upward position
+    if ((theta_old - targetedFinalTheta) * (theta - targetedFinalTheta) <= 0) {
+      targetThetaCrossed = true;
+    }
+    
+    else{
+      targetThetaCrossed = false;
+    }
+
+
+    // attemp the speed up to maintain stability if unstable
+    if (!decelerationPeriod && adjustmentPeriod){
+
+      if (theta < targetedFinalTheta){
+        motorPWM = motorMaxSpeed;
+        initialPWMbeforeDeceleration = motorMaxSpeed;
+      }
+
+      if (theta > targetedFinalTheta){
+        motorPWM = -motorMaxSpeed;
+        initialPWMbeforeDeceleration = -motorMaxSpeed;
+      }
+
+    }
+
+
+    //case for upward position crossed on the left side
+    if (adjustmentPeriod && targetThetaCrossed && currentSide == 0){
+
+      decelerationPeriod = true;
+      adjustmentPeriod = false;
+
+      if(theta > targetedFinalTheta){
+        x_i = x;
+        x_f = tolerable_x_min;
+        decelerationPointMarked = true;
+      }
+
+      if(theta <= targetedFinalTheta){
+        x_i = x;
+        x_f = centerDistance;
+        decelerationPointMarked = true;
+      }
+
+    }
+
+    //decelerate according to the markings
+    if (decelerationPeriod && decelerationPointMarked){
+      decelerateTo(100 , x, x_i, x_f);
+    }
+
+
+    //stop decelerating and go to adjustment when theta is not maintaining upward position well
+    if (decelerationPeriod && abs(theta - targetedFinalTheta) > toleranceAngle){
+      decelerationPeriod = false;
+      decelerationPointMarked = false;
+      adjustmentPeriod = true;
+    }
+
+    //case for upward position crossed on the right side
+    if (adjustmentPeriod && targetThetaCrossed && currentSide == 1){
+
+      decelerationPeriod = true;
+      adjustmentPeriod = false;
+
+      if(theta > targetedFinalTheta){
+        x_i = x;
+        x_f = tolerable_x_max;
+        decelerationPointMarked = true;
+      }
+
+      if(theta <= targetedFinalTheta){
+        x_i = x;
+        x_f = centerDistance;
+        decelerationPointMarked = true;
+      }
+
+    }
+    
+    //safety stop
+    if (adjustmentPeriod && (x < 0.03 || x > 0.65)){
+      isMoving = false;
+    }
+
+    //flags for when deceleration is completed
+    if (x_f > x_i && x >= x_f){
+      adjustmentPeriod = true;
+      decelerationPeriod = false;
+      decelerationPointMarked = false;
+    }
+
+    if (x_f < x_i && x <= x_f){
+      adjustmentPeriod = true;
+      decelerationPeriod = false;
+      decelerationPointMarked = false;
+    }
+
+    Serial.println(motorPWM);
+
+    
   }
 
-
-
-
-
-
-
-
-
-
-
-
-  
   // safe gaurd
-  if (!isMoving || safegaurd){
+  if (!isMoving){
     motorPWM = 0; 
   }
 
@@ -244,6 +340,7 @@ void measureTimeVariantVariables(){
   lastMeasureTime = currentTime;
 } 
 
+
 //Calculation based on initial conditions and final condition
 void calculateEnergyEquation(double theta_initial, double dtheta_initial, double theta_final, double x_initial, double dx_initial, double x_final){
 
@@ -266,13 +363,12 @@ void calculateEnergyEquation(double theta_initial, double dtheta_initial, double
 
   x_begin = x_initial;
   x_end =  x_final;
-  // x_end_decel = 10 * (x_end - x_begin) + x_begin;
 }
 
-//create acceleration by changing veocity on distance; CANNOT DECELERATE ONLY ACCELERATE IN ONE DIRECTION AT A TIME
+//create acceleration by changing veocity on distance
 void accelerateTo(double finalVelocity, double x_current, double x_initial, double x_final){
-  double PWM_final = abs(finalVelocity) * 176.81 + 80;
-  motorPWM = int( PWM_final - PWM_final * abs( (x_final - x_current) / ( x_final - x_initial ) ) + 120);
+  double PWM_final = abs(finalVelocity) * 176.81 + 65;
+  motorPWM = int( PWM_final - PWM_final * abs( (x_final - x_current) / ( x_final - x_initial ) ) + 150);
 
   //deadzone control
   if (motorPWM > 255){
@@ -287,6 +383,27 @@ void accelerateTo(double finalVelocity, double x_current, double x_initial, doub
   }
 
 }
+
+void decelerateTo(double initialPWM, double x_current, double x_initial, double x_final){
+  double pwmDeadzone = 75;
+  if (initialPWM > 0 && x_final > x_initial){
+    motorPWM = (initialPWM - pwmDeadzone) / (x_initial - x_final) * (x_current - x_initial) + initialPWM;
+  }
+
+  if (initialPWM < 0 && x_final < x_initial){
+    pwmDeadzone *= -1;
+    motorPWM = (initialPWM - pwmDeadzone) / (x_initial - x_final) * (x_current - x_initial) + initialPWM;
+  }
+
+}
+
+double calculateDecelerationMagnitude(double initialPWM, double x_final, double x_initial ){
+  double velocity = ( abs(initialPWM) - 80 ) / 176.81 ;
+  double decelerateMagnitude = abs( (pow(velocity,3)) / (3 * (x_final - x_initial) ) );
+  return(decelerateMagnitude);
+}
+
+
 
 //create acceleration and deceleration according to sine wave function
 void sineWaveAccelerate(double peakVelocity, double x_current, double x_initial, double x_final) {
@@ -303,11 +420,6 @@ void sineWaveAccelerate(double peakVelocity, double x_current, double x_initial,
     if (x_current > x_final){
       motorPWM = deadzoneShift;
     }
-
-    if (x_current > x_final + 0.2){
-      motorPWM = 0;
-    }
-
   }
 
   if (x_final < x_initial){
@@ -315,12 +427,7 @@ void sineWaveAccelerate(double peakVelocity, double x_current, double x_initial,
     if (motorPWM < 0 && motorPWM > -deadzoneShift){
       motorPWM = -deadzoneShift;
     }
-
-    if (x_current < x_final - 0.2){
-      motorPWM = 0;
-    }
   }
-
 }
 
 
@@ -380,66 +487,5 @@ void printLog(){
   Serial.print("| PWM = "); Serial.print(motorPWM); Serial.print(" ");
   Serial.print("| isMoving = "); Serial.print((isMoving)? "true":"false");
   Serial.print("| isSwinging = "); Serial.print((isSwinging)? "true":"false");
-  Serial.print("| beginStablization = "); Serial.print((beginStablization)? "true":"false");
-  Serial.print("| returnToCenter = "); Serial.print((returnToCenter)? "true":"false");
-  Serial.print("| extended_end = "); Serial.print(( 2*(x_end - x_begin) + x_begin )); Serial.println(" m ");
+  Serial.print("| beginStablization = "); Serial.println((beginStablization)? "true":"false");
 }
-
-
-
-
-
-
-
-
-// if (returnToCenter && !calculated && abs(theta-targetedFinalTheta) > radians(1)) {
-//       calculateEnergyEquation( (theta > targetedFinalTheta)? theta + radians(30): theta - radians(30) , dtheta, targetedFinalTheta, x, dx, centerDistance);
-//       isMoving = true;
-//       calculated = true;
-//     }
-    
-//     //stop moving flags when returning to center
-//     if (returnToCenter && calculated && isMoving){
-//       if (x_begin < centerDistance && x >= centerDistance){
-//         calculated = false;
-//         returnToCenter = false;
-//       }
-//       if (x_begin > centerDistance && x <= centerDistance){
-//         calculated = false;
-//         returnToCenter = false;
-//       }
-//     }
-
-//     if (isMoving && returnToCenter && calculated){
-//       x_end_decel = 2 * (x_end - x_begin) + x_begin;
-//       sineWaveAccelerate(final_dx_new, x, x_begin, centerDistance);
-//     }
-
-//     if (!returnToCenter && !calculated){
-//       calculateEnergyEquation( theta - radians(30), dtheta, targetedFinalTheta, x, dx, x + (l * sin(theta) / 2 ));
-//       isMoving = true;
-//       calculated = true;
-//     }
-
-//     if (isMoving && !returnToCenter && calculated){
-//       sineWaveAccelerate(final_dx_new, x, x_begin, x_end);
-//     }
-    
-//     if (isMoving && !returnToCenter && calculated){
-//       if ( x_end_decel > x_begin && x >= x_end_decel && motorPWM == 40){
-//         isMoving = false;
-//         calculated = false;
-//         returnToCenter = true;
-//     }
-//       if ( x_end_decel < x_begin && x <= x_end_decel && motorPWM == -40){
-//         isMoving = false;
-//         calculated = false;
-//         returnToCenter = true;
-//       }
-//     }
-
-//   }
-  
-//   if (theta > 2*PI || theta < -2*PI ){
-//     safegaurd = true;
-
